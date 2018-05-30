1316fed6be12a7e740e01f87977f026a0af88a07
Thomas Bouchard <thomaswardbouchard@gmail.com>
2018-05-30 15:31:42 +0200

    cleanup

--- thijs/react-ex1/src/components/ImageList/Image/Image.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Image extends Component {
-  render() {
-    return <img src={this.props.imageSrc} width="200" height="200" />
-  }
-};
-
-export default Image;
